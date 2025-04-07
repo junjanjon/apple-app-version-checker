5aa26adc5d67d796627458199589d5f9cf1e5b75
Junki Hiroi <junki.hiroi@drecom.co.jp>
2025-04-07 12:01:05 +0900

    Update lint

--- src/fetchAppInfo.test.ts
+++ src/fetchAppInfo.test.ts
@@ -3,41 +3,46 @@ import axios from 'axios';
 import fetchAppInfo from './fetchAppInfo';
 
 
-describe('fetchAppInfo', () => {
-  it('should fetch app information successfully', async () => {
+describe('fetchAppInfo', () =>
+{
+  it('should fetch app information successfully', async () =>
+  {
     const mockCountryCode = 'jp';
     const mockAppId = '123456789';
     const mockVerbose = false;
     const mockFormat = 'json';
 
     // Mock console.log to capture output
-    const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+    const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() =>
+    {});
 
-      (axios.get as any).mockResolvedValue({ data: `<html>
+    (axios.get as any).mockResolvedValue({ data: `<html>
         <script name="schema:software-application">{"name":"Test App"}</script>
         <div class="whats-new__latest__version">バージョン 1.0.0</div>
         <time data-test-we-datetime datetime="2025-04-07"></time>
         </html>` });
 
-      
+
     await fetchAppInfo(mockCountryCode, mockAppId, mockVerbose, mockFormat);
 
     expect(consoleLogSpy).toHaveBeenCalledWith(
-      JSON.stringify({ appName: 'Test App', appVersion: '1.0.0', releaseDate: '2025-04-07' })
+      JSON.stringify({ appName: 'Test App', appVersion: '1.0.0', releaseDate: '2025-04-07' }),
     );
 
     // Restore mocked functions
     consoleLogSpy.mockRestore();
   });
 
-  it('should handle errors gracefully', async () => {
+  it('should handle errors gracefully', async () =>
+  {
     const mockCountryCode = 'jp';
     const mockAppId = 'invalid';
     const mockVerbose = false;
     const mockFormat = 'json';
 
     // Mock console.error to capture error output
-    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() =>
+    {});
 
     // Mock axios to simulate an error response
     (axios.get as any).mockRejectedValue(new Error('Network Error'));
